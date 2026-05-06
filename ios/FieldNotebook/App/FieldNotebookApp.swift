6dfa55624bcd19bf152611950220fe73ad001690
Mirosław Zaniewicz <mirekzaniewicz@icloud.com>
2026-05-06 15:31:25 +0200

    Bootstrap SwiftUI Xcode project with iOS 17 target via XcodeGen

---
+++ ios/FieldNotebook/App/FieldNotebookApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct FieldNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Field Notebook")
+                .font(.title)
+        }
+    }
+}
